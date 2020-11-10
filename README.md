87dbc2c432c804c0efdf0af359301588fd399940
Vic Romero <vromerocomino@gmail.com>
2020-11-10 17:16:37 +0100

    Add books functionality done

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# mylibrary
+# Project: Library
+
+This is a project for https://www.theodinproject.com [https://www.theodinproject.com/courses/javascript/lessons/library]
+
+Live demo: https://vic-ro.github.io/mylibrary/
+
+Github repository: https://github.com/vic-Ro/mylibrary/
+
+The idea of the project was to set up a Library where the user could add Books, introduce their information through a form,
+and display them in some sort of table or each on their own "card". The user had to be able to switch any book between
+"Read" and "Unread" status.
+
+I also decided to practice with Drag and Drop and LocalStorage. Any Book added or deleted would be saved between sessions, 
+and the user can swap book cards to sort them.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,133 @@
+// Define Book function constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
-const myLibrary = [
-  new Book('Lord of the Rings', 'Tolkien', 298, true),
-  new Book('Don Quixote', 'Cervantes', 436, false),
-  new Book('Hammlet', 'Shakespeare', 569, true),
-  new Book('One hundred years', 'Gabriel Garcia Marquez', 328, false),
-  new Book('In search of lost time', 'Marcel Proust', 128, true),
-  new Book('Mountains of Madness', 'H.P Lovecraft', 20, false),
-  new Book('War and peace', 'Leo Tolstoy', 368, true),
-  new Book('Lolita', 'Vladimir Nabokov', 1878, false),
-  new Book('Crime and punishment', 'Fyodor Dostoyevsky', 156, false),
-  new Book('Pride and prejudice', 'Jane Austen', 300, true),
-];
+// Fill the library with the first books
+let myLibrary = [];
 
+if (localStorage.key('myLibrary')) {
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+} else {
+  myLibrary = [
+    new Book('The Hobbit', 'J.R.R. Tolkien', 310, false),
+    new Book('Don Quixote', 'Miguel de Cervantes', 863, false),
+    new Book('Hamlet', 'William Shakespeare', 164, false),
+    new Book('In search of lost time', 'Marcel Proust', 4215, false),
+  ];
+}
+
+/** Generates a book card. Adding Drag and Drop functionality, a delete button for the card
+ *  and read/unread button interaction. */
 const dropzone = document.getElementById('drop-zone');
 
-function displayBooks(array) {
-  array.forEach((element) => {
-    const myBook = document.createElement('div');
-    if (element.read === true) {
-      myBook.innerHTML = `<h2 class="book-title">${element.title}</h2><ul class="book-info"><li class="">by ${element.author}</li><li class="">${element.pages} pages</li><li class="">Read</li></ul><div class='relative'><i class="fas fa-trash-alt"></i>`;
-    } else {
-      myBook.innerHTML = `<h2 class="book-title">${element.title}</h2><ul class="book-info"><li class="">by ${element.author}</li><li class="">${element.pages} pages</li><li class="">Not read yet</li></ul><div class='relative'><i class="fas fa-trash-alt"></i>`;
-    }
-    myBook.classList.add('grid-item');
-    myBook.classList.add('draggable');
-    myBook.setAttribute('draggable', 'true');
-    dropzone.appendChild(myBook);
-    myBook.addEventListener('dragstart', () => {
-      myBook.classList.add('dragging');
-    });
-    myBook.addEventListener('dragend', () => {
-      myBook.classList.remove('dragging');
-    });
-    const deleteButton = myBook.lastChild.lastChild;
-    deleteButton.addEventListener('click', () => {
-      const parent = deleteButton.parentElement.parentElement;
-      parent.remove();
-    });
+function addDragDrop(div) {
+  div.addEventListener('dragstart', () => {
+    div.classList.add('dragging');
+  });
+  div.addEventListener('dragend', () => {
+    div.classList.remove('dragging');
   });
 }
 
+function addDeleteButton(div) {
+  const deleteButton = div.lastChild.lastChild;
+  deleteButton.addEventListener('click', () => {
+    const parent = deleteButton.parentElement.parentElement;
+    const bookTitle = parent.children[0].textContent;
+    const updateLibrary = myLibrary.filter((book) => (book.title !== bookTitle));
+    myLibrary = updateLibrary;
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    parent.remove();
+  });
+}
+
+function addReadButton(div) {
+  const button = div.children[1].children[2].children[0];
+  button.addEventListener('click', () => {
+    if (button.classList.contains('read')) {
+      button.classList.remove('read');
+      button.classList.add('not-read');
+      // eslint-disable-next-line no-param-reassign
+      button.textContent = 'Unread';
+      const bookTitle = div.children[0].textContent;
+      const bookIndex = myLibrary.findIndex((book) => (book.title === bookTitle));
+      myLibrary[bookIndex].read = false;
+      localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    } else {
+      button.classList.remove('not-read');
+      button.classList.add('read');
+      // eslint-disable-next-line no-param-reassign
+      button.textContent = 'Read';
+      const bookTitle = div.children[0].textContent;
+      const bookIndex = myLibrary.findIndex((book) => (book.title === bookTitle));
+      myLibrary[bookIndex].read = true;
+      localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    }
+  });
+}
+
+function createBookCard(book) {
+  const bookDiv = document.createElement('div');
+  if (book.read === true) {
+    bookDiv.innerHTML = `<h2 class="book-title">${book.title}</h2><ul class="book-info"><li>by ${book.author}</li><li>${book.pages} pages</li><li><button class='read read-button'>Read</button></li></ul><div class='relative'><i class="fas fa-trash-alt"></i></div>`;
+  } else {
+    bookDiv.innerHTML = `<h2 class="book-title">${book.title}</h2><ul class="book-info"><li>by ${book.author}</li><li>${book.pages} pages</li><li><button class='not-read read-button'>Unread</button></li></ul><div class='relative'><i class="fas fa-trash-alt"></i></div>`;
+  }
+  bookDiv.classList.add('grid-item');
+  bookDiv.classList.add('draggable');
+  bookDiv.setAttribute('draggable', 'true');
+  dropzone.appendChild(bookDiv);
+
+  addDragDrop(bookDiv);
+  addDeleteButton(bookDiv);
+  addReadButton(bookDiv);
+}
+
+// First display when the page is loaded
+function displayBooks(array) {
+  array.forEach((book) => {
+    createBookCard(book);
+  });
+}
+
+displayBooks(myLibrary);
+
+// Button and emerging window for adding a new book through the page
+const addNewBook = document.querySelector('.menu-title');
+const formWindow = document.querySelector('.emerging-window');
+
+addNewBook.addEventListener('click', () => {
+  formWindow.classList.toggle('hidden');
+});
+
+// Adding a book through the form
+const addBookButton = document.getElementById('form-button');
+
+addBookButton.addEventListener('click', () => {
+  const myForm = document.getElementById('add-book_form');
+  const title = document.getElementById('form-title').value;
+  const author = document.getElementById('form-author').value;
+  const pages = document.getElementById('form-pages').value;
+  const read = document.getElementById('form-read').checked;
+  if (title === '' || author === '' || pages === '') {
+    return;
+  }
+  // eslint-disable-next-line radix
+  myLibrary.push(new Book(title, author, parseInt(pages), read));
+  createBookCard(myLibrary[myLibrary.length - 1]);
+  myForm.reset();
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  formWindow.classList.toggle('hidden');
+});
+
+const closeWindow = document.querySelector('.fa-times');
+
+closeWindow.addEventListener('click', () => formWindow.classList.toggle('hidden'));
+
+// Drag and drop interaction for the book cards
 function swapNodes(a, b) {
   const allItems = [...document.querySelectorAll('.draggable')];
   const positionDragged = allItems.indexOf(a) + 1;
@@ -74,5 +157,3 @@ dropzone.addEventListener('drop', (e) => {
   if (e.target.parentElement.classList !== undefined) e.target.parentElement.classList.remove('draggingover');
   if (e.target.parentElement.parentElement.classList !== undefined) e.target.parentElement.parentElement.classList.remove('draggingover');
 });
-
-displayBooks(myLibrary);
